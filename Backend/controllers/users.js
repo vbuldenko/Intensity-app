fc1313f006f26ac5acaf0da31b60f8be4ca27150
Vladyslav Buldenko <vladyslavbuldenko@Vladyslavs-Laptop.local>
2023-09-29 23:24:43 +0300

    some changes

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -3,55 +3,53 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-    const users = await User.find({}).populate("blogs", {
-        url: 1,
-        title: 1,
-        author: 1,
-        likes: 1,
-    });
-    response.json(users);
+  const users = await User.find({});
+  response.json(users);
 });
 
 usersRouter.get("/:id", async (request, response) => {
-    const users = await User.findById(request.params.id).populate("blogs", {
-        title: 1,
-        author: 1,
-        url: 1,
-        likes: 1,
-    });
+  const users = await User.findById(request.params.id).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
 
-    response.json(users);
+  response.json(users);
 });
 
 usersRouter.post("/", async (request, response, next) => {
-    const { username, name, password } = request.body;
-    if (!password || !username) {
-        return response
-            .status(400)
-            .json({ error: "Both username and password are required!" });
-    }
+  const { username, name, surname, email, phone, password } = request.body;
+  if (!password || !username || !surname || !email || !name) {
+    return response
+      .status(400)
+      .json({ error: "All form details are required!" });
+  }
 
-    if (password.length < 3) {
-        return response
-            .status(400)
-            .json({ error: "Password should be at least 3 characters long!" });
-    }
+  if (password.length < 4) {
+    return response
+      .status(400)
+      .json({ error: "Password should be at least 4 characters long!" });
+  }
 
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    });
+  const user = new User({
+    username,
+    name,
+    surname,
+    email,
+    phone,
+    passwordHash,
+  });
 
-    try {
-        const savedUser = await user.save();
-        response.status(201).json(savedUser);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter;
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -2,26 +2,37 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        minLength: 3,
-        unique: true,
-    },
-    name: String,
-    passwordHash: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true,
+  },
+  name: String,
+  surname: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: String,
 });
 
 userSchema.plugin(uniqueValidator);
 
 userSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-        // the passwordHash should not be revealed
-        delete returnedObject.passwordHash;
-    },
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
 });
 
 const User = mongoose.model("User", userSchema);
