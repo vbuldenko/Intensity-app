ab364902b5e075333d58917d8706c6e4f1f65eba
Vladyslav Buldenko <vladyslavbuldenko@Vladyslavs-Laptop.local>
2023-11-05 19:52:46 +0200

    purchases of abonements partly implemented

--- Backend/controllers/abonements.js
+++ Backend/controllers/abonements.js
@@ -3,150 +3,145 @@ const Abonement = require("../models/abonement");
 const userExtractor = require("../utils/middleware").userExtractor;
 
 abonementRouter.get("/", async (request, response, next) => {
-    try {
-        const abonements = await Abonement.find({});
-        response.json(abonements);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const abonements = await Abonement.find({});
+    response.json(abonements);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // get abonnements by user ID
 abonementRouter.get("/user", userExtractor, async (request, response, next) => {
-    const { user } = request;
-    try {
-        if (!user) {
-            return response
-                .status(401)
-                .json({ error: "operation not permitted" });
-        }
-
-        const abonnements = await Abonement.find({ user: user.id }).populate(
-            "history"
-        );
-        response.json(abonnements);
-    } catch (error) {
-        next(error);
+  const { user } = request;
+  try {
+    if (!user) {
+      return response.status(401).json({ error: "operation not permitted" });
     }
+
+    const abonnements = await Abonement.find({ user: user.id }).populate(
+      "history"
+    );
+    response.json(abonnements);
+  } catch (error) {
+    next(error);
+  }
 });
 
 abonementRouter.get("/:id", async (request, response, next) => {
-    try {
-        const abonement = await Abonement.findById(request.params.id);
-        if (abonement) {
-            response.json(abonement);
-        } else {
-            response.status(404).end();
-        }
-    } catch (error) {
-        next(error);
+  try {
+    const abonement = await Abonement.findById(request.params.id);
+    if (abonement) {
+      response.json(abonement);
+    } else {
+      response.status(404).end();
     }
+  } catch (error) {
+    next(error);
+  }
 });
 
 abonementRouter.post("/", userExtractor, async (request, response, next) => {
-    const { body, user } = request;
+  const { body, user } = request;
 
-    try {
-        if (!user) {
-            return response
-                .status(401)
-                .json({ error: "operation not permitted" });
-        }
-        //responds with status 400 if there no title or url properties in the request
-        if (!body.amount || body.amount === 0) {
-            return response
-                .status(400)
-                .json({ error: "Amount of days in abonement is not choosed" });
-        }
-
-        //Adding properties not declared in the first place
-        body.purchase_date = new Date();
-        body.paused = false;
-        body.left = body.amount;
-
-        const abonement = new Abonement({ ...body, user: user.id }); //Why here user.id and not user._id? Why there no error? Lookup later for better understanding!!!
-        const savedAbonement = await abonement.save();
-        user.abonements = user.abonements.concat(savedAbonement._id);
-        await user.save();
-        response.status(201).json(savedAbonement);
-    } catch (error) {
-        console.log(body);
-        next(error);
+  try {
+    if (!user) {
+      return response.status(401).json({ error: "operation not permitted" });
     }
+    //responds with status 400 if there no title or url properties in the request
+    if (!body.amount || body.amount === 0) {
+      return response
+        .status(400)
+        .json({ error: "Amount of days in abonement is not choosed" });
+    }
+
+    //Adding properties not declared in the first place
+    body.purchase_date = new Date();
+    body.paused = false;
+    body.left = body.amount;
+
+    const abonement = new Abonement({ ...body, user: user.id }); //Why here user.id and not user._id? Why there no error? Lookup later for better understanding!!!
+    const savedAbonement = await abonement.save();
+    console.log(savedAbonement);
+    user.abonements = user.abonements.concat(savedAbonement._id);
+    await user.save();
+    response.status(201).json(savedAbonement);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 abonementRouter.put("/:id", userExtractor, async (request, response, next) => {
-    const abonementId = request.params.id;
-    const { body, user } = request;
-    const { updateType } = body;
+  const abonementId = request.params.id;
+  const { body, user } = request;
+  const { updateType } = body;
 
-    try {
-        if (!user) {
-            return response
-                .status(401)
-                .json({ error: "operation not permitted" });
-        }
-
-        const abonement = await Abonement.findById(abonementId);
-        if (!abonement) {
-            return response.status(404).json({ error: "Abonement not found." });
-        }
-
-        if (updateType === "activation" && !abonement.activation_date) {
-            const currentDate = new Date();
-            // Calculate the expiration date by adding one month to the current date
-            const expirationDate = new Date(currentDate);
-            expirationDate.setMonth(currentDate.getMonth() + 1);
-
-            abonement.activation_date = currentDate;
-            abonement.expiration_date = expirationDate;
-        } else if (updateType === "activation" && abonement.activation_date) {
-            return response
-                .status(401)
-                .json({ error: "abonement already activated" });
-        }
-
-        if (new Date(abonement.expirationDate) > new Date()) {
-            return response
-                .status(401)
-                .json({ error: "abonement expired. Buy a new one!" });
-        }
-
-        if (updateType === "reservation") {
-            abonement.left -= 1; //deduct 1 training from active abonement
-            abonement.history.push(body.trainingId); //wy not _id?
-        } else if (updateType === "cancellation") {
-            abonement.left += 1; //return 1 training to active abonement
-            abonement.history = abonement.history.filter(
-                (id) => id.toString() !== body.trainingId
-            );
-        }
-
-        // const update = {
-        //     $set: {
-        //         // Specify the new values
-        //         ...abonementBody,
-        //     },
-        // };
-
-        // const updatedAbonement = await Abonement.findByIdAndUpdate(
-        //     abonementId,
-        //     update,
-        //     {
-        //         new: true,
-        //         runValidators: true,
-        //         context: "query",
-        //     }
-        // );
-        const savedAbonement = await abonement.save();
-        const populatedAbonement = await Abonement.findById(
-            savedAbonement.id
-        ).populate("history");
-        console.log(savedAbonement);
-        response.json(populatedAbonement);
-    } catch (error) {
-        next(error);
+  try {
+    if (!user) {
+      return response.status(401).json({ error: "operation not permitted" });
     }
+
+    const abonement = await Abonement.findById(abonementId);
+    if (!abonement) {
+      return response.status(404).json({ error: "Abonement not found." });
+    }
+
+    if (updateType === "activation" && !abonement.activation_date) {
+      const currentDate = new Date();
+      // Calculate the expiration date by adding one month to the current date
+      const expirationDate = new Date(currentDate);
+      expirationDate.setMonth(currentDate.getMonth() + 1);
+
+      abonement.activation_date = currentDate;
+      abonement.expiration_date = expirationDate;
+    } else if (updateType === "activation" && abonement.activation_date) {
+      return response
+        .status(401)
+        .json({ error: "abonement already activated" });
+    }
+
+    if (new Date(abonement.expirationDate) > new Date()) {
+      return response
+        .status(401)
+        .json({ error: "abonement expired. Buy a new one!" });
+    }
+
+    if (updateType === "reservation") {
+      abonement.left -= 1; //deduct 1 training from active abonement
+      abonement.history.push(body.trainingId); //wy not _id?
+    } else if (updateType === "cancellation") {
+      abonement.left += 1; //return 1 training to active abonement
+      abonement.history = abonement.history.filter(
+        (id) => id.toString() !== body.trainingId
+      );
+    }
+
+    // const update = {
+    //     $set: {
+    //         // Specify the new values
+    //         ...abonementBody,
+    //     },
+    // };
+
+    // const updatedAbonement = await Abonement.findByIdAndUpdate(
+    //     abonementId,
+    //     update,
+    //     {
+    //         new: true,
+    //         runValidators: true,
+    //         context: "query",
+    //     }
+    // );
+    const savedAbonement = await abonement.save();
+    const populatedAbonement = await Abonement.findById(
+      savedAbonement.id
+    ).populate("history");
+    console.log(savedAbonement);
+    response.json(populatedAbonement);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // abonementRouter.delete(
--- FrontendVite/src/components/User/Purchases.jsx
+++ FrontendVite/src/components/User/Purchases.jsx
@@ -1,73 +1,110 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { notifyWith } from '../../reducers/notificationReducer';
 import { createAbonement } from '../../reducers/abonementReducer';
 
-export default function Purchases() {
-    const [abonementData, setAbonementData] = useState({
-        amount: 4,
-        activation: false,
-    });
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(abonementData);
-    }
-
-    function handleChange(e) {
-        const { name, type, value, checked } = e.target;
-        const newValue = type === 'checkbox' ? checked : value;
-
-        setAbonementData((prev) => ({
-            ...prev,
-            [name]: newValue,
-        }));
-    }
-
+function IndividualAbonement({ amount, price }) {
     const dispatch = useDispatch();
+    const calculatePricePerTraining = () => price / amount;
 
-    // const handleLogin = async (event) => {
-    //     event.preventDefault();
-    //     try {
-    //         dispatch(signUserIn({ username, password }));
-    //         setUsername('');
-    //         setPassword('');
-    //         navigate('/');
-    //     } catch (exception) {
-    //         dispatch(notifyWith({ text: 'Wrong credentials', error: true }));
-    //     }
-    // };
+    const handleSubmit = async () => {
+        const abonementData = { amount };
+        try {
+            dispatch(createAbonement({ amount }));
+            dispatch(
+                notifyWith(`Abonement for ${amount} trainings was purchased`)
+            );
+        } catch (error) {
+            dispatch(notifyWith(error.response.data.error));
+        }
+    };
+
+    const abonementStyle = {
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        padding: '20px',
+        marginBottom: '20px',
+        backgroundColor: 'white',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+        display: 'flex',
+        flexDirection: 'column',
+    };
+
+    const buttonStyle = {
+        backgroundColor: 'blue',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        marginTop: '10px',
+    };
 
     return (
-        <div className="login-wrapper">
-            <form className="abon-purchase-form" onSubmit={handleSubmit}>
-                <h1>Choose your abonement</h1>
-                <div>
-                    <input
-                        id="amount"
-                        type="number"
-                        value={abonementData.amount}
-                        name="amount"
-                        placeholder="amount"
-                        onChange={handleChange}
-                    />
+        <div style={abonementStyle}>
+            <h2>Abonement for {amount} Trainings</h2>
+            <p>Price: ${price}</p>
+            <p>Price per 1 Training: ${calculatePricePerTraining()}</p>
+            <button style={buttonStyle} onClick={handleSubmit}>
+                Purchase
+            </button>
+        </div>
+    );
+}
+
+export default function Purchases() {
+    const notification = useSelector(({ notification }) => notification);
+    const abonementData = [
+        { amount: 4, price: 1300 },
+        { amount: 6, price: 1800 },
+        { amount: 8, price: 2200 },
+        { amount: 10, price: 2500 },
+        { amount: 12, price: 2700 },
+    ];
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <h1 style={{ fontSize: '2rem', marginBottom: '20px' }}>
+                Choose your Abonement
+            </h1>
+            {notification && (
+                <div
+                    style={{
+                        color: 'red',
+                        textAlign: 'center',
+                        padding: '0.5em',
+                    }}
+                >
+                    {notification}
                 </div>
-                <div style={{ display: 'flex' }}>
-                    <input
-                        id="activation"
-                        type="checkbox"
-                        checked={abonementData.activation}
-                        name="activation"
-                        onChange={handleChange}
-                        style={{ display: 'block' }}
-                    />
-                    <label htmlFor="activation"> Activate</label>
-                </div>
-                <button id="purchase-button" type="submit">
-                    Purchase
-                </button>
-            </form>
+            )}
+
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: '1em',
+                    justifyContent: 'center',
+                }}
+            >
+                {abonementData.map(({ amount, price }) => (
+                    <div
+                        key={amount}
+                        style={{
+                            flex: '1',
+                            maxWidth: '300px',
+                        }}
+                    >
+                        <IndividualAbonement amount={amount} price={price} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
