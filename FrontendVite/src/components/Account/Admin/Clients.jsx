4dab9d26954e7a6ed9f78e3eec6754bb4725b20c
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-01-12 14:43:18 +0200

    admin style mods

--- FrontendVite/src/components/Account/Admin/Clients.jsx
+++ FrontendVite/src/components/Account/Admin/Clients.jsx
@@ -34,39 +34,44 @@ export default function Clients(props) {
 
     return (
         <div className="clients-container">
-            <div className="header">
-                <div className="all-metric">
-                    <p>All Clients: {clients.length}</p>
-                    {/* <p className="active-clients">
+            <div className="sub-header">
+                <div className="selector">
+                    <div className="clients-all">
+                        <p>All: {clients.length}</p>
+                        {/* <p className="active-clients">
                         Active Clients: {clients.active}
                     </p> */}
+                    </div>
+                    <div className="button-divider"></div>
+                    <div className="view-modes">
+                        <button
+                            className={`view-button ${
+                                viewMode === 'tiles' ? 'active' : ''
+                            }`}
+                            onClick={() => handleViewChange('view', 'tiles')}
+                        >
+                            <Squares2X2Icon className="h-4 w-4" />
+                        </button>
+                        <button
+                            className={`view-button ${
+                                viewMode === 'list' ? 'active' : ''
+                            }`}
+                            onClick={() => handleViewChange('view', 'list')}
+                        >
+                            <ListBulletIcon className="h-4 w-4" />
+                        </button>
+                    </div>
                 </div>
+
+                {/* <div className="all-metric"></div> */}
                 <div className="search">
-                    <MagnifyingGlassIcon className="h-6 w-6" />
+                    <MagnifyingGlassIcon className="h-4 w-4" />
                     <input
                         type="text"
                         placeholder="Search clients..."
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
-                </div>
-                <div className="view-modes">
-                    <button
-                        className={`view-button ${
-                            viewMode === 'list' ? 'active' : ''
-                        }`}
-                        onClick={() => handleViewChange('view', 'list')}
-                    >
-                        <ListBulletIcon className="h-6 w-6" />
-                    </button>
-                    <button
-                        className={`view-button ${
-                            viewMode === 'tiles' ? 'active' : ''
-                        }`}
-                        onClick={() => handleViewChange('view', 'tiles')}
-                    >
-                        <Squares2X2Icon className="h-6 w-6" />
-                    </button>
                 </div>
             </div>
             <div className={`clients-list ${viewMode}`}>
@@ -77,14 +82,28 @@ export default function Clients(props) {
                         className={`client ${viewMode}`}
                         state={{ search: `?${searchParams.toString()}` }}
                     >
-                        <p className="client-name">
-                            {client.name} {client.surname}
-                        </p>
-                        <p className="client-phone">{client.phone}</p>
-                        <p className="client-email">{client.email}</p>
-                        <p className="client-details">
-                            Abonements: {client.abonements.length}
-                        </p>
+                        <div className="client-title">
+                            <p className="client-name">
+                                {client.name} {client.surname}
+                            </p>
+                            <p className="client-status">Active</p>
+                        </div>
+                        <div className="client-data">
+                            <div>
+                                <p>Phone</p>
+                                <p className="client-phone">{client.phone}</p>
+                            </div>
+                            <div>
+                                <p>Mail</p>
+                                <p className="client-email">{client.email}</p>
+                            </div>
+                            <div>
+                                <p>Abonement</p>
+                                <p className="client-details">
+                                    {client.abonements.length}
+                                </p>
+                            </div>
+                        </div>
                     </Link>
                 ))}
             </div>
