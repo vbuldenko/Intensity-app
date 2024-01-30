98e982798b52f36c295c6eaaaab3bc021b43653a
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-01-30 15:41:32 +0200

    changes in abonement(reservations) BE and FE

--- Backend/controllers/abonements.js
+++ Backend/controllers/abonements.js
@@ -3,210 +3,224 @@ const Abonement = require("../models/abonement");
 const userExtractor = require("../utils/middleware").userExtractor;
 
 abonementRouter.get("/", async (request, response, next) => {
-  try {
-    const abonements = await Abonement.find({}).populate({
-      path: "user",
-      select: "surname name",
-    });
+    try {
+        const abonements = await Abonement.find({}).populate({
+            path: "user",
+            select: "surname name",
+        });
 
-    response.json(abonements);
-  } catch (error) {
-    next(error);
-  }
+        response.json(abonements);
+    } catch (error) {
+        next(error);
+    }
 });
 
 // get abonnements by user ID
 abonementRouter.get("/user", userExtractor, async (request, response, next) => {
-  const { user } = request;
-  try {
-    if (!user) {
-      return response.status(401).json({ error: "operation not permitted" });
+    const { user } = request;
+    try {
+        if (!user) {
+            return response
+                .status(401)
+                .json({ error: "operation not permitted" });
+        }
+
+        const abonements = await Abonement.find({ user: user.id }).populate({
+            path: "history",
+            populate: {
+                path: "instructor",
+                select: "surname",
+            },
+        });
+
+        response.json(abonements);
+    } catch (error) {
+        next(error);
     }
-
-    const abonements = await Abonement.find({ user: user.id }).populate({
-      path: "history",
-      populate: {
-        path: "instructor",
-        select: "surname",
-      },
-    });
-
-    response.json(abonements);
-  } catch (error) {
-    next(error);
-  }
 });
 
 abonementRouter.get("/:id", async (request, response, next) => {
-  try {
-    const abonement = await Abonement.findById(request.params.id);
-    if (abonement) {
-      response.json(abonement);
-    } else {
-      response.status(404).end();
+    try {
+        const abonement = await Abonement.findById(request.params.id);
+        if (abonement) {
+            response.json(abonement);
+        } else {
+            response.status(404).end();
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 });
 
 abonementRouter.post("/", userExtractor, async (request, response, next) => {
-  const { body, user } = request;
+    const { body, user } = request;
 
-  try {
-    if (!user) {
-      return response.status(401).json({ error: "operation not permitted" });
+    try {
+        if (!user) {
+            return response
+                .status(401)
+                .json({ error: "operation not permitted" });
+        }
+
+        const abonements = await Abonement.find({ user: user.id });
+        const isAvailableForUse = abonements.find(
+            (abonement) =>
+                abonement.status === "active" ||
+                abonement.status === "non-active"
+        );
+
+        if (isAvailableForUse) {
+            return response
+                .status(400)
+                .json({ error: "You already have an abonement" });
+        }
+        //responds with status 400 if there no title or url properties in the request
+        if (!body.amount || body.amount === 0) {
+            return response
+                .status(400)
+                .json({ error: "Amount of days in abonement is not choosed" });
+        }
+
+        //Adding properties not declared in the first place
+        body.purchase_date = new Date();
+        body.paused = false;
+        body.left = body.amount;
+        body.status = "non-active";
+
+        const abonement = new Abonement({ ...body, user: user.id }); //Why here user.id and not user._id? Why there no error? Lookup later for better understanding!!!
+        const savedAbonement = await abonement.save();
+        console.log(savedAbonement);
+        user.abonements = user.abonements.concat(savedAbonement._id);
+        await user.save();
+        response.status(201).json(savedAbonement);
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-
-    const abonements = await Abonement.find({ user: user.id });
-    const activeAbonement = abonements.find((abonement) => {
-      const expirationDate = new Date(abonement.expiration_date);
-      return abonement.expiration_date
-        ? expirationDate >= new Date()
-        : !abonement.expiration_date;
-    });
-
-    if (activeAbonement) {
-      return response
-        .status(400)
-        .json({ error: "You already have an abonement" });
-    }
-    //responds with status 400 if there no title or url properties in the request
-    if (!body.amount || body.amount === 0) {
-      return response
-        .status(400)
-        .json({ error: "Amount of days in abonement is not choosed" });
-    }
-
-    //Adding properties not declared in the first place
-    body.purchase_date = new Date();
-    body.paused = false;
-    body.left = body.amount;
-
-    const abonement = new Abonement({ ...body, user: user.id }); //Why here user.id and not user._id? Why there no error? Lookup later for better understanding!!!
-    const savedAbonement = await abonement.save();
-    console.log(savedAbonement);
-    user.abonements = user.abonements.concat(savedAbonement._id);
-    await user.save();
-    response.status(201).json(savedAbonement);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 });
 
 abonementRouter.put("/:id", userExtractor, async (request, response, next) => {
-  const abonementId = request.params.id;
-  const { body, user } = request;
-  const { updateType } = body;
+    const abonementId = request.params.id;
+    const { body, user } = request;
+    const { updateType } = body;
 
-  try {
-    if (!user) {
-      return response.status(401).json({ error: "operation not permitted" });
-    }
+    try {
+        if (!user) {
+            return response
+                .status(401)
+                .json({ error: "operation not permitted" });
+        }
 
-    if (user.role !== "client") {
-      return response
-        .status(401)
-        .json({ error: "Unauthorized: You are not in client role! (1)" });
-    }
+        if (user.role !== "client") {
+            return response.status(401).json({
+                error: "Unauthorized: You are not in client role! (1)",
+            });
+        }
 
-    const abonement = await Abonement.findById(abonementId);
-    if (!abonement) {
-      return response.status(404).json({ error: "Abonement not found." });
-    }
+        const abonement = await Abonement.findById(abonementId);
+        if (!abonement) {
+            return response.status(404).json({ error: "Abonement not found." });
+        }
 
-    if (user.id !== abonement.user.toString()) {
-      return response.status(400).json({ error: "not your abonement" });
-    }
+        if (user.id !== abonement.user.toString()) {
+            return response.status(400).json({ error: "not your abonement" });
+        }
 
-    if (updateType === "activation" && !abonement.activation_date) {
-      const currentDate = new Date();
-      // Calculate the expiration date by adding one month to the current date
-      const expirationDate = new Date(currentDate);
-      expirationDate.setMonth(currentDate.getMonth() + 1);
+        if (updateType === "activation" && !abonement.activation_date) {
+            const currentDate = new Date();
+            // Calculate the expiration date by adding one month to the current date
+            const expirationDate = new Date(currentDate);
+            expirationDate.setMonth(currentDate.getMonth() + 1);
 
-      abonement.activation_date = currentDate;
-      abonement.expiration_date = expirationDate;
-    } else if (updateType === "activation" && abonement.activation_date) {
-      return response
-        .status(401)
-        .json({ error: "abonement already activated" });
-    }
+            abonement.activation_date = currentDate;
+            abonement.expiration_date = expirationDate;
+            abonement.status = "active";
+        } else if (updateType === "activation" && abonement.activation_date) {
+            return response
+                .status(401)
+                .json({ error: "abonement already activated" });
+        }
 
-    if (new Date(abonement.expiration_date) < new Date()) {
-      return response
-        .status(401)
-        .json({ error: "abonement expired. Buy a new one!" });
-    }
+        if (new Date(abonement.expiration_date) < new Date()) {
+            return response
+                .status(401)
+                .json({ error: "abonement expired. Buy a new one!" });
+        }
 
-    if (updateType === "freeze" && !abonement.paused) {
-      abonement.paused = true;
-      // Calculate new expiration date by adding one week
-      const newExpirationDate = new Date(abonement.expiration_date);
-      newExpirationDate.setDate(newExpirationDate.getDate() + 7);
-      abonement.expiration_date = newExpirationDate;
-    } else if (updateType === "freeze" && abonement.paused) {
-      return response
-        .status(400)
-        .json({ error: "abonement was already paused!" });
-    }
+        if (updateType === "freeze" && !abonement.paused) {
+            abonement.paused = true;
+            // Calculate new expiration date by adding one week
+            const newExpirationDate = new Date(abonement.expiration_date);
+            newExpirationDate.setDate(newExpirationDate.getDate() + 7);
+            abonement.expiration_date = newExpirationDate;
+        } else if (updateType === "freeze" && abonement.paused) {
+            return response
+                .status(400)
+                .json({ error: "abonement was already paused!" });
+        }
 
-    if (updateType === "reservation") {
-      if (abonement.history.includes(body.trainingId)) {
-        return response.status(400).json({
-          error: "Already reserved: You have already reserved your place!",
+        if (updateType === "reservation") {
+            if (abonement.history.includes(body.trainingId)) {
+                return response.status(400).json({
+                    error: "Already reserved: You have already reserved your place!",
+                });
+            }
+
+            if (!abonement.activation_date) {
+                const currentDate = new Date();
+                // Calculate the expiration date by adding one month to the current date
+                const expirationDate = new Date(currentDate);
+                expirationDate.setMonth(currentDate.getMonth() + 1);
+
+                abonement.activation_date = currentDate;
+                abonement.expiration_date = expirationDate;
+                abonement.status = "active";
+            }
+            abonement.left -= 1; //deduct 1 training from active abonement
+            if (abonement.left === 0) {
+                abonement.status = "ended"; //change status of abonement
+            }
+            abonement.history.push(body.trainingId); //wy not _id?
+        } else if (updateType === "cancellation") {
+            if (abonement.left === 0) {
+                abonement.status = "active"; //change status of abonement
+            }
+            abonement.left += 1; //return 1 training to active abonement
+            abonement.history = abonement.history.filter(
+                (id) => id.toString() !== body.trainingId
+            );
+        }
+
+        // const update = {
+        //     $set: {
+        //         // Specify the new values
+        //         ...abonementBody,
+        //     },
+        // };
+
+        // const updatedAbonement = await Abonement.findByIdAndUpdate(
+        //     abonementId,
+        //     update,
+        //     {
+        //         new: true,
+        //         runValidators: true,
+        //         context: "query",
+        //     }
+        // );
+        const savedAbonement = await abonement.save();
+        const populatedAbonement = await Abonement.findById(
+            savedAbonement.id
+        ).populate({
+            path: "history",
+            populate: {
+                path: "instructor",
+            },
         });
-      }
-
-      if (!abonement.activation_date) {
-        const currentDate = new Date();
-        // Calculate the expiration date by adding one month to the current date
-        const expirationDate = new Date(currentDate);
-        expirationDate.setMonth(currentDate.getMonth() + 1);
-
-        abonement.activation_date = currentDate;
-        abonement.expiration_date = expirationDate;
-      }
-      abonement.left -= 1; //deduct 1 training from active abonement
-      abonement.history.push(body.trainingId); //wy not _id?
-    } else if (updateType === "cancellation") {
-      abonement.left += 1; //return 1 training to active abonement
-      abonement.history = abonement.history.filter(
-        (id) => id.toString() !== body.trainingId
-      );
+        console.log(savedAbonement);
+        response.json(populatedAbonement);
+    } catch (error) {
+        next(error);
     }
-
-    // const update = {
-    //     $set: {
-    //         // Specify the new values
-    //         ...abonementBody,
-    //     },
-    // };
-
-    // const updatedAbonement = await Abonement.findByIdAndUpdate(
-    //     abonementId,
-    //     update,
-    //     {
-    //         new: true,
-    //         runValidators: true,
-    //         context: "query",
-    //     }
-    // );
-    const savedAbonement = await abonement.save();
-    const populatedAbonement = await Abonement.findById(
-      savedAbonement.id
-    ).populate({
-      path: "history",
-      populate: {
-        path: "instructor",
-      },
-    });
-    console.log(savedAbonement);
-    response.json(populatedAbonement);
-  } catch (error) {
-    next(error);
-  }
 });
 
 // abonementRouter.delete(
--- Backend/models/abonement.js
+++ Backend/models/abonement.js
@@ -1,37 +1,38 @@
 const mongoose = require("mongoose");
 
 const abonementSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  purchase_date: { type: Date, required: true },
-  activation_date: { type: Date },
-  expiration_date: { type: Date },
-  paused: Boolean,
-  left: Number,
-  history: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Training",
+    status: String,
+    amount: {
+        type: Number,
+        required: true,
     },
-  ],
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
+    price: {
+        type: Number,
+        required: true,
+    },
+    purchase_date: { type: Date, required: true },
+    activation_date: { type: Date },
+    expiration_date: { type: Date },
+    paused: Boolean,
+    left: Number,
+    history: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Training",
+        },
+    ],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
 });
 
 abonementSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
 module.exports = mongoose.model("Abonement", abonementSchema);
