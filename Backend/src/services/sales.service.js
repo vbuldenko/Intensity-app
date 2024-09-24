e6a1a239d4f50d07556588174609631e88e0f474
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-09-24 10:49:12 +0300

    ts project separated

--- Backend/src/services/sales.service.js
+++
@@ -1,9 +0,0 @@
-// const Sales = require("../models/sales");
-const { collectStatistics } = require("../utils/statisticsCollector");
-
-const get = async () => {
-  // const sales = await Sales.find({});
-  return await collectStatistics();
-};
-
-module.exports = { get };
--- Frontend/src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-import classNames from 'classnames';
-
-export const getLinkClass = ({ isActive }) =>
-    classNames({
-        active: isActive,
-    });
