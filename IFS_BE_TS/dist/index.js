9508ee6dd59f692f222ba34e29e9d9c790c7d00f
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-09-24 15:32:02 +0300

    Folder rename

--- IFS_BE_TS/dist/index.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from './app';
-const PORT = process.env.PORT || 10000;
-const app = createApp();
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-});
-//# sourceMappingURL=index.js.map
