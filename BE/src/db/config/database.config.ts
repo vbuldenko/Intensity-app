0fe97eb916bf67fa05dab1a33b7f649a50892137
Vladyslav <vladbuldenko@gmail.com>
2024-10-17 01:16:34 +0300

    BE: render db use

--- BE/src/db/config/database.config.ts
+++ BE/src/db/config/database.config.ts
@@ -21,10 +21,12 @@ interface Config {
 
 const config: Config = {
   development: {
-    username: process.env.DB_USER || 'dev_user',
-    password: process.env.DB_PASS || 'dev_pass',
-    database: process.env.DB_NAME || 'dev_db',
-    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER || 'fsadmin',
+    password: process.env.DB_PASS || '5pYiaw9DBCTnsruaoWylw8djfF0ltWbP',
+    database: process.env.DB_NAME || 'dbifs',
+    host:
+      process.env.DB_HOST ||
+      'dpg-cs7nbpt6l47c73c69eh0-a.frankfurt-postgres.render.com',
     port: Number(process.env.DB_PORT) || 5432,
     dialect: 'postgres',
     logging: console.log,
