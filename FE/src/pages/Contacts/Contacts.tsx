d7a1e28a16b1abf1669849bebcf72c61b9d72dc3
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-11-07 15:05:51 +0200

    FE: Localization of Contacts

--- FE/src/pages/Contacts/Contacts.tsx
+++ FE/src/pages/Contacts/Contacts.tsx
@@ -4,41 +4,41 @@ import {
   MapPinIcon,
   ClockIcon,
 } from "@heroicons/react/24/outline";
+import { useTranslation } from "react-i18next";
 
 export default function ContactsPage() {
+  const { t } = useTranslation();
   return (
     <section className="info-section">
-      <h2 className="info-section__header">Наші контакти</h2>
+      {/* <h2 className="info-section__header">{t("contacts.header")}</h2> */}
       <div className="info-section__list">
         <div className="info-section__item card-element">
           <h4 className="info-section__title accent-label-primary top-0 absolute">
-            Наші контакти
+            {t("contacts.header")}
           </h4>
           <div className="info-section__content mt-8">
             <div className="info-section__element">
               <PhoneIcon className="icon" />
-              <p>+38(097)-99-100-70</p>
+              <p>{t("contacts.phone")}</p>
             </div>
             <div className="info-section__element">
               <EnvelopeIcon className="icon" />
-              <p>in10sity.trainer@gmail.com</p>
+              <p>{t("contacts.email")}</p>
             </div>
             <div className="info-section__element">
               <MapPinIcon className="icon" />
-              <p>Volodymyra Ivasiuka Ave, 24, Kyiv, 04210</p>
+              <p>{t("contacts.address")}</p>
             </div>
           </div>
         </div>
         <div className="info-section__item card-element">
           <h4 className="info-section__title accent-label-primary top-0 absolute">
-            Графік роботи
+            {t("contacts.schedule")}
           </h4>
           <div className="info-section__content mt-8">
             <div className="info-section__element">
               <ClockIcon className="icon" />
-              <p>
-                Пн-Пт - з 9:00 до 21:00 <br /> Сб - з 9:00 до 15:00
-              </p>
+              <p>{t("contacts.working_hours")}</p>
             </div>
           </div>
         </div>
