889676b4ca4857d80bf017ea4cacaec6cfcc7234
Vladyslav <vladbuldenko@gmail.com>
2024-11-09 13:48:21 +0200

    FE: Admin update

--- FE/src/pages/Account/Overview/Trainer/TrainerOverview.tsx
+++ FE/src/pages/Account/Overview/Trainer/TrainerOverview.tsx
@@ -7,12 +7,14 @@ import {
 } from "../../../../utils/trainings";
 import "./TrainerOverview.scss";
 import { User } from "../../../../types/User";
+import { useTranslation } from "react-i18next";
 
 interface TrainerOverviewProps {
   user: User;
 }
 
 const TrainerOverview: React.FC<TrainerOverviewProps> = ({ user }) => {
+  const { t } = useTranslation();
   const currentDate = new Date();
   const trainerTrainings = filterByVisitors(user.trainings, 2);
 
@@ -30,22 +32,30 @@ const TrainerOverview: React.FC<TrainerOverviewProps> = ({ user }) => {
   return (
     <div className="trainer-overview">
       <div className="trainer-overview__salary-section card-element">
-        <h3 className="trainer-overview__title">Salary</h3>
+        <h3 className="trainer-overview__title">
+          {t("trainerOverview.salary")}
+        </h3>
 
         <div className="trainer-overview__result">
-          <p className="trainer-overview__label">Current total</p>
+          <p className="trainer-overview__label">
+            {t("trainerOverview.currentTotal")}
+          </p>
           <span className="trainer-overview__value text-teal-600">
             {getCurrentWage(currentMonthTrainings)} ₴
           </span>
         </div>
         <div className="trainer-overview__result">
-          <p className="trainer-overview__label">Today</p>
+          <p className="trainer-overview__label">
+            {t("trainerOverview.today")}
+          </p>
           <span className="trainer-overview__value text-teal-600">
             {getCurrentWage(currentDayTrainings)} ₴
           </span>
         </div>
         <div className="trainer-overview__result">
-          <p className="trainer-overview__label">Month trainings</p>
+          <p className="trainer-overview__label">
+            {t("trainerOverview.monthTrainings")}
+          </p>
           <span className="trainer-overview__value">
             {currentMonthTrainings.length}
           </span>
@@ -53,7 +63,9 @@ const TrainerOverview: React.FC<TrainerOverviewProps> = ({ user }) => {
       </div>
 
       <div className="trainer-overview__trainings-section card-element">
-        <h3 className="trainer-overview__title">Today Trainings</h3>
+        <h3 className="trainer-overview__title">
+          {t("trainerOverview.todayTrainings")}
+        </h3>
         <div className="trainer-overview__trainings-list">
           {currentDayTrainings.map((training) => (
             <div key={training.id} className="training">
@@ -65,12 +77,16 @@ const TrainerOverview: React.FC<TrainerOverviewProps> = ({ user }) => {
                 </div>
                 <div>
                   <p className="training__visitors">
-                    Visitors: {training.visitors.length}
+                    {t("history.visitors")}: {training.visitors.length}
                   </p>
                 </div>
               </div>
             </div>
-          ))}
+          )).length > 0 || (
+            <p className="text-gray-500 text-center">
+              {t("trainerOverview.noTrainings")}
+            </p>
+          )}
         </div>
       </div>
     </div>
