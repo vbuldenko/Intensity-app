e35a3545e7981e32bd4c375468f52fc913b22d9c
Vladyslav <vladbuldenko@gmail.com>
2024-11-09 13:23:16 +0200

    FE: Admin update

--- FE/src/components/Elements/Search/Search.tsx
+++ FE/src/components/Elements/Search/Search.tsx
@@ -1,5 +1,6 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import "./Search.scss";
+import { useTranslation } from "react-i18next";
 
 interface SearchInputProps {
   value: string;
@@ -7,12 +8,13 @@ interface SearchInputProps {
 }
 
 export default function SearchInput({ value, onChange }: SearchInputProps) {
+  const { t } = useTranslation();
   return (
     <div className="search">
       <MagnifyingGlassIcon className="icon search__icon" />
       <input
         type="text"
-        placeholder="Search clients..."
+        placeholder={t("search.placeholder")}
         value={value}
         onChange={(e) => onChange(e.target.value)}
         className="search__input"
