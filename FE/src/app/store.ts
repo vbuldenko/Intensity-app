4817fc903435d575a6ca04723b85f562f81996a1
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-12-23 11:46:47 +0200

    FE: Redux store update to clear state upon logout

--- FE/src/app/store.ts
+++ FE/src/app/store.ts
@@ -1,20 +1,33 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./features/user/userSlice";
 import authReducer from "./features/auth/authSlice";
 import trainingReducer from "./features/trainings/trainingSlice";
 import abonementReducer from "./features/abonements/abonementSlice";
+import { logOut } from "./features/auth/authThunk";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    user: userReducer,
-    trainings: trainingReducer,
-    abonements: abonementReducer,
-  },
+const appReducer = combineReducers({
+  auth: authReducer,
+  user: userReducer,
+  trainings: trainingReducer,
+  abonements: abonementReducer,
 });
 
-// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+const rootReducer = (
+  state: ReturnType<typeof appReducer> | undefined,
+  action: Action
+) => {
+  if (action.type === logOut.fulfilled.type) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+// Infer the `RootState`, `AppDispatch`, and `AppStore` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export type AppStore = typeof store;
