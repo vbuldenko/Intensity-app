037fcae0e5fc40e1303635175de2d85833cb75fd
Vladyslav <vladbuldenko@gmail.com>
2025-05-05 19:09:37 +0300

    FE: Prices update

--- FE/src/assets/pricesData.ts
+++ FE/src/assets/pricesData.ts
@@ -3,28 +3,20 @@ export const prices = [
     category: "Group",
     prices: [
       { amount: 1, price: 400 },
-      { amount: 4, price: 1400 },
-      { amount: 6, price: 2000 },
-      { amount: 8, price: 2400 },
-      { amount: 10, price: 2700 },
-      { amount: 12, price: 3000 },
+      { amount: 4, price: 1500 },
+      { amount: 6, price: 2100 },
+      { amount: 8, price: 2600 },
+      { amount: 10, price: 3000 },
+      { amount: 12, price: 3300 },
     ],
   },
   {
     category: "Individual",
-    prices: [
-      { amount: 1, price: 750 },
-      { amount: 5, price: 3500 },
-      { amount: 10, price: 6000 },
-    ],
+    prices: [{ amount: 1, price: 1000 }],
   },
   {
     category: "Split",
-    prices: [
-      { amount: 1, price: 1000 },
-      { amount: 5, price: 4500 },
-      { amount: 10, price: 8000 },
-    ],
+    prices: [{ amount: 1, price: 1500 }],
   },
   // {
   //   category: "Children",
--- FE/src/assets/purchaseData.ts
+++ FE/src/assets/purchaseData.ts
@@ -1,21 +1,13 @@
 export const membership = {
   group: [
-    { amount: 1, price: 250 },
+    { amount: 1, price: 275 },
     { amount: 1, price: 400 },
-    { amount: 4, price: 1400 },
-    { amount: 6, price: 2000 },
-    { amount: 8, price: 2400 },
-    { amount: 10, price: 2700 },
-    { amount: 12, price: 3000 },
+    { amount: 4, price: 1500 },
+    { amount: 6, price: 2100 },
+    { amount: 8, price: 2600 },
+    { amount: 10, price: 3000 },
+    { amount: 12, price: 3300 },
   ],
-  personal: [
-    { amount: 1, price: 750 },
-    { amount: 5, price: 3500 },
-    { amount: 10, price: 6000 },
-  ],
-  split: [
-    { amount: 1, price: 1000 },
-    { amount: 5, price: 4500 },
-    { amount: 10, price: 8000 },
-  ],
+  personal: [{ amount: 1, price: 1000 }],
+  split: [{ amount: 1, price: 1500 }],
 };
